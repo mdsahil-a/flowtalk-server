ff0e375d4584f9b0d761ba1b2d33655a4ba2e716
Sahil <73626982+mdsahil-a@users.noreply.github.com>
2026-01-03 20:03:22 +0530

    Change sameSite attribute for JWT cookies
    Updated cookie settings for JWT in signup and login functions to use 'none' for sameSite attribute.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -53,7 +53,7 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
+      sameSite: "none", // prevent CSRF attacks
       secure: true,
     });
 
@@ -85,7 +85,7 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
+      sameSite: "none", // prevent CSRF attacks
       secure: true,
     });
 
@@ -148,4 +148,5 @@ export async function onboard(req, res) {
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
+
 
